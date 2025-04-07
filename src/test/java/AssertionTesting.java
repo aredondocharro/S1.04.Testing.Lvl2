e14601d53d020843fae469f235c991cee711b4d7
Alejandro Redondo Charro <alejandro.redondo.charro@gmail.com>
2025-04-07 12:59:23 +0200

    Some modifications

--- src/test/java/AssertionTesting.java
+++ src/test/java/AssertionTesting.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.*;
